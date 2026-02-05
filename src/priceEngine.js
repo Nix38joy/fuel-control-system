52606157de8cd7fac02b773a5e9fe947b03c478c
German Zhumabaev <ger32390man@gmail.com>
2026-02-05 21:55:50 +0300

    feat: add calculateFuelLimit function with template strings

---
+++ src/priceEngine.js
@@ -0,0 +1,28 @@
+function calculateFuelLimit(money, fuelType, hasCard) {
+    let minSum;
+    let min98;
+
+    if (hasCard) {
+        minSum = 300;
+        min98 = 1500;
+    } else {
+        minSum = 500;
+        min98 = 2000;
+    }
+
+    if (money < minSum) {
+        return `Минимальная сумма заправки ${minSum} р`;
+    } 
+    else if (fuelType === "98" && money < min98) {
+        return `Для 98-го бензина мин. сумма ${min98} р`;
+    } 
+    else if (fuelType === "diesel" && money < 1000) {
+        return `Минимальная сумма для дизеля 1000 р`;
+    }
+    else if (fuelType === "92" || fuelType === "95") {
+        return "Заправка разрешена, ожидайте";
+    } 
+    else {
+        return "Ошибка: несуществующий вид топлива";
+    }
+}
